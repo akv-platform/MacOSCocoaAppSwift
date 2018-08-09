fb5c38b7b92338c077f880315324299c11833f59
Sergey Dolin <v-sedoli@microsoft.com>
2018-08-09 21:36:22 +0500

    Initial Commit

---
+++ MacOSCocoaAppSwift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacOSCocoaAppSwift
+//
+//  Created by Sergey Dolin on 09/08/2018.
+//  Copyright © 2018 Sergey Dolin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacOSCocoaAppSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacOSCocoaAppSwift
+//
+//  Created by Sergey Dolin on 09/08/2018.
+//  Copyright © 2018 Sergey Dolin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
